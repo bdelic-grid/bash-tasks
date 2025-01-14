7054fa98d79b1f606dba4241ea17e96f69625651
Bosko Delic <bdelic@griddynamics.com>
2025-01-14 11:35:48 +0100

    add fizz buzz

---
+++ 3-fizz.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for ((i=1; i<=100; i++)); do
+	if [[ $(( $i % 3 )) -eq 0 && $(( $i % 5 )) -eq 0 ]]; then
+		echo "$i: Fizz Buzz"
+	elif [[ $(( $i % 3 )) -eq 0 ]]; then
+		echo "$i: Fizz"
+	else
+		echo "$i: Buzz"
+	fi
+done
+
+
