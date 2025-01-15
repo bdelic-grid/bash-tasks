d313693e754e954bb2e33867aa3bc427d31de504
Bosko Delic <bdelic@griddynamics.com>
2025-01-15 12:05:27 +0100

    add 2-ops

---
+++ 2-ops.sh
@@ -0,0 +1,42 @@
+#!/bin/bash 
+
+while getopts "o:n:d" opt; do
+		case $opt in
+				o)
+						OPERATOR="$OPTARG"
+						;;
+				n)
+						array+=("$OPTARG")
+						while [ "$OPTIND" -le "$#" ] && [ "${!OPTIND:0:1}" != "-" ]; do
+								array+=("${!OPTIND}")
+								OPTIND="$(expr "$OPTIND" \+ 1)"
+						done
+						res="${array[0]}"
+
+						for ((i=1; i<${#array[@]}; i++)); do
+								num="${array[$i]}"
+								case $OPERATOR in
+										"+") res=$(( res + num ))
+												;;
+										"-") res=$(( res - num ))
+												;;
+										"*") res=$(( res * num ))
+												;;
+										"%") res=$(( res % num ))
+												;;
+								esac
+						done
+						echo "Result: $res"
+						;;
+				d)
+						USER=$(whoami)
+						echo "User: $USER"
+						echo "Script: $0"
+						echo "Operation: $OPERATOR"
+						echo "Numbers: " "${array[@]}"
+						;;
+				\?)
+						echo "Invalid argument"
+						;;
+		esac
+done
